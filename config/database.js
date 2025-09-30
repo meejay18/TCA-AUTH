8ce15edab85c5e099426ad78b72fcf7842d6524d
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-09-30 14:15:45 +0100

    updated

--- config/database.js
+++ config/database.js
@@ -2,29 +2,29 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const DB = process.env.MONGO_URL
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Database connected')
-  })
-  .catch((err) => {
-    console.log(`Error connecting to database: ${err.message}`)
-  })
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Database connected')
+//   })
+//   .catch((err) => {
+//     console.log(`Error connecting to database: ${err.message}`)
+//   })
 
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(DB, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     console.log('MongoDB connected successfully')
-//   } catch (error) {
-//     console.error('MongoDB connection failed:', error.message)
-//     process.exit(1)
-//   }
-// }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('MongoDB connected successfully')
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message)
+    process.exit(1)
+  }
+}
 
-// connectDB()
+connectDB()
