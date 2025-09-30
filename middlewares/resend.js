ae2d7bcd53bcdb1b235db92d6f0fc9bae48d902a
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-09-30 14:11:09 +0100

    updated

--- middlewares/resend.js
+++ middlewares/resend.js
@@ -1,17 +1,17 @@
-const { Resend } = require('resend')
+// const { Resend } = require('resend')
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+// const resend = new Resend(process.env.RESEND_API_KEY)
 
-exports.resendEmail = async (options) => {
-  try {
-    await resend.emails.send({
-      from: options.from || 'Mije <onboarding@resend.dev>',
-      to: options.email,
-      subject: options.subject,
-      html: options.html,
-    })
-    console.log('Message sent')
-  } catch (error) {
-    console.log(error.message)
-  }
-}
+// exports.resendEmail = async (options) => {
+//   try {
+//     await resend.emails.send({
+//       from: options.from || 'Mije <onboarding@resend.dev>',
+//       to: options.email,
+//       subject: options.subject,
+//       html: options.html,
+//     })
+//     console.log('Message sent')
+//   } catch (error) {
+//     console.log(error.message)
+//   }
+// }
