9862329b5d0bfd93b9185dd6e205e814fbc9606f
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-09-30 14:35:45 +0100

    updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   })
 })
 
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server running on port ${process.env.PORT}`)
-// })
-module.exports = app
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+})
+// module.exports = app
